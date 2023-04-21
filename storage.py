dd3f33de0daf1dda99bbe4baf7830b7547cc439d
juan-maringka <123565849+juan-maringka@users.noreply.github.com>
2023-04-21 15:18:41 +0000

    added some methods

--- storage.py
+++ storage.py
@@ -1,4 +1,5 @@
 import sqlite3
+DBNAME = "webapp_database"
 
 class Collection:
     """
@@ -10,8 +11,8 @@ class Collection:
     update(name, record)
     delete(name)
     """
-    def __init__(self, dbname, tblname):
-        self._dbname = dbname
+    def __init__(self, tblname):
+        self._dbname = DBNAME
         self._tblname = tblname
 
     def __repr__(self):
@@ -24,49 +25,119 @@ class Collection:
         conn.commit()
         conn.close()
 
-    def add(self, record: dict):
-        """Adds a record into the table"""
-        return record
-        
-    def get(self, name: str):
-        """Returns a record with the corresponding name from collection"""
-        return record
+    def _return(self, query, values=None):
+        conn = sqlite3.connect(self._dbname)
+        c = conn.cursor()
+        c.execute(query, values)
+        rows = c.fetchall() 
+        conn.close()
+        return rows
 
-    def update(self, name: str, record: dict):
-        """Updates a record with the corresponding name from collection"""
-        return
-
-    def delete(self, name: str):
-        """Deletes the record with the corresponding name"""
-        return
-
-#junction table class needs to be updated to support multiple junction tables
-class JunctionTable(Collection):
-    """
-    """
-    def __init__(self):
-        super().__init__("JunctionTable")
 
 class Students(Collection):
     """
     Student Collection
+
+    Methods:
+    --------
+    add(record)
+    get(student_name)
+    update(student_name, record)
+    delete(student_name)
     """
     def __init__(self):
-        super().__init__("Webapp_Database", "Students")
+        super().__init__("Students")
 
     def add(self, record: dict):
+        """
+        Adds a student record into the database.
+        Record includes
+        """
         query = f"""
-                INSERT INTO {self._tblname} VALUES (?, ?, ?, ?, ?)
+                INSERT INTO {self._tblname} VALUES (?, ?, ?, ?, ?, ?, ?)
                 """
         values = tuple(record.values())
         self._execute(query, values)
 
+    def get(self, student_name):
+        """Returns record of student."""
+        query = f"""
+                SELECT *
+                FROM {self._tblname}
+                """
+
+    def update(self, student_name, record):
+        """Updates student record in database."""
+        pass
+
+    def delete(self, student_name):
+        """Deletes student record from database."""
+        pass
+
+
 class Classes(Collection):
     """
     Classes Collection
+
+    Methods:
+    --------
+    add()
+    get_all(class_name)
+    update()
+    delete()
     """
     def __init__(self):
         super().__init__("Classes")
+
+    def add(self, name):
+        """"""
+        pass
+
+    def get_all(self, class_name):
+        """Returns all students in the corresponding class."""
+        # check if class exists
+        query = f"""
+                SELECT 'name'
+                FROM 'sqlite_master'
+                WHERE type = 'table' AND name = ?
+                """
+        values = tuple(class_name)
+        rows = self._return(query, values)
+        if not rows[0][0]:
+            return None
+            
+        # retrieve class_id
+        query = f"""
+                SELECT 'class_id'
+                FROM '{self._tblname}'
+                WHERE class_name = ?
+                """
+        values = tuple(class_name)
+        rows = self._return(query, values)
+        class_id = rows[0]    # tuple
+        
+        # retrieve student_id and student_name
+        query = f"""
+                SELECT 'student_Id', 'student_name'
+                FROM 'Students-Classes'
+                WHERE class_id = ?
+                ORDER BY 'student_id' ASC
+                """
+        values = class_id
+        rows = self._return(query, values)
+        
+        data = {}
+        for item in rows:
+            data[item[0]] = item[1]
+        return data    # dictionary(key=id, value=student_name)
+
+    def update(self):
+        """"""
+        pass
+
+    def delete(self, class_name):
+        """"""
+        pass
 
 
 class Subjects(Collection):
@@ -77,13 +148,66 @@ class Subjects(Collection):
         super().__init__("Subjects")
 
 
-class Ccas(Collection):
+class CCAs(Collection):
     """
     CCAs Collection
     """
     def __init__(self):
         super().__init__("CCAs")
 
+    def add(self, cca_name):
+        """"""
+        pass
+
+    def search(self, student_name):
+        """Returns a student's CCA."""
+        # retrieve student_id
+        query = """
+                SELECT 'student_id'
+                FROM 'Students'
+                WHERE student_name = ?
+                """
+        values = tuple(student_name)
+        rows = self._return(query, values)
+        student_id = rows[0]    # tuple
+
+        # retrieve cca_id
+        query = """
+                SELECT 'cca_id'
+                FROM 'Students-CCAs'
+                WHERE student_id = ?
+                """
+        values = student_id
+        rows = self._return(query, values)
+        cca_id = rows[0]    # tuple
+
+        # retrive cca_name
+        query = """
+                SELECT 'cca_name'
+                FROM 'Ccas'
+                WHERE cca_id = ?
+                """
+        values = cca_id
+        rows = self._return(query, values)
+        cca_name = rows[0][0]
+
+        data = {}
+        data[student_name] = cca_name
+        return data    # dictionary(key=student_name, value=cca_name)
+
+    def get_all(self, cca_name):
+        """Returns a CCA's details."""
+        query = """
+                SELECT *
+                FROM 'CCAs'
+                WHERE cca_name = ?
+                """
+        values = tuple(cca_name)
+        rows = self._return(query, values)
+        ##############################
+
+
+    # get_all method returns cca info
 
 class Activities(Collection):
     """
@@ -92,3 +216,5 @@ class Activities(Collection):
     def __init__(self):
         super().__init__("Activies")
 
+    # search method return dictionary {kaelin: activity}
+    # get_all method returns activity info
