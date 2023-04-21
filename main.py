0e68e3c0380396bfcfee9ce87f8e9e41a0d7bf8f
kqjy29 <124025795+kqjy29@users.noreply.github.com>
2023-04-21 15:46:29 +0000

    i think i completed most of it just need to insert juan's methods and make it look nicer i guess it works u can test?

--- main.py
+++ main.py
@@ -96,7 +96,7 @@ def add():
                            tdtype = tdtype
                           )
 
-@app.route('/view')
+@app.route('/view', methods=['GET', 'POST'])
 def view():
     page_type = 'new'
     title = 'What would you like to view?'
@@ -107,16 +107,26 @@ def view():
     }
     form_data={'choice': ''}
     data = {}
+    choice = ''
+    key = ''
 
     if request.args.get('choice') in choices:
         choice = request.args.get('choice')
-        page_type = 'result'
-        title = 'The information is as follows:'
+        page_type = 'search'
+        title = f'Please enter the {choice} you would like to search for:'
         ## .get the appropriate data from idk where juan pls help
-        data={
-                'Student Name':'Kaelin',
-                'Student CCA': 'Band'
-            }
+        form_meta={
+        'action':'/view?searched',
+        'method': 'post'
+    }
+
+    if 'searched' in request.args:
+        # get data from databases, data u get will be a dictionary, use the search method
+        # assign it to data and it should work
+        key = list(request.form.keys()) 
+        data = {'name':'band', 'type': 'music', 'cca_id': '12'}
+        title = f'{key[0]}: {request.form[key[0]]}'
+        page_type = 'result'
         
     return render_template('view.html',
                           choices = choices,
@@ -124,12 +134,72 @@ def view():
                           form_meta = form_meta,
                           form_data = form_data,
                           data = data,
-                          title = title)
+                          title = title,
+                          choice = choice,
+                          key = key)
 
 
-@app.route('/edit')
+@app.route('/edit', methods=['GET', 'POST'])
 def edit():
-    return render_template('edit.html')
+    page_type = 'new'
+    title = 'Would you like to edit a CCA or an activity?'
+    choices = ['CCA', 'Activity']
+    form_meta={
+        'action':'/edit?edit',
+        'method': 'get'
+    }
+    choice = ''
+    data = {}
+    key = ''
+    name = ''
+    new = ''
+
+    if request.args.get('choice') in choices:
+        choice = request.args.get('choice')
+        page_type = 'search'
+        title = f'Please enter the name of the student you would like to change the {choice} of:'
+        form_meta = {
+            'action':'/edit?searched',
+            'method': 'post'
+        }
+
+    if 'searched' in request.args:
+        key = list(request.form.keys())
+        page_type = 'verify'
+        choice = request.form['choice']
+        data = {'kaelin':'band'}
+        # need if statement for either activity or cca and do the .search .find wtv
+        if data is None:
+            title = 'Unable to edit, student does not exist'
+            page_type = ''
+        else:         
+            title = 'Please edit the record below'
+            form_meta = {
+                'action':'/edit?success',
+                'method': 'post'
+            }
+
+    if 'success' in request.args:
+        # do the editing
+        # name is just the student name
+        # new is either the new cca or new activity
+        name = request.form['name']
+        new = request.form['edit']
+        page_type = 'success'
+        title = 'The following record has been updated!'
+        choice = request.form['choice']
+        
+    return render_template('edit.html',
+                          page_type = page_type,
+                          title = title,
+                          choice = choice,
+                          form_meta = form_meta,
+                          choices = choices,
+                          data = data,
+                          key = key,
+                          name = name,
+                          new = new)
+
 
 if __name__ == '__main__':
     app.run('0.0.0.0')
