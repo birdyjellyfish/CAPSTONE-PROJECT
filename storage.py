31d3c229b224d97194a684f4fc37bc3e22a0c1ba
juan-maringka <123565849+juan-maringka@users.noreply.github.com>
2023-04-22 17:07:09 +0000

    added _retrieve helper function and modified _return

--- storage.py
+++ storage.py
@@ -21,11 +21,27 @@ class Collection:
 
     def _return(self, query, values=None):
         conn = sqlite3.connect(self._dbname)
+        conn.row_factory = sqlite3.Row
         c = conn.cursor()
         c.execute(query, values)
-        rows = c.fetchall() 
+        row = c.fetchone() 
         conn.close()
-        return rows
+        return row # sqlite3.Row object (supports both numerical and key indexing)
+
+    def _retrieve(self, a, b, c, d):
+        """
+        Returns a query and values to retrieve data
+
+        SELECT 'a'
+        FROM 'b'
+        WHERE c = 'd'
+        """
+        query = f"""
+                SELECT '{a}'
+                FROM '{b}'
+                WHERE {c} = '{d}'
+                """
+        return query
 
 
 class Students(Collection):
@@ -87,31 +103,28 @@ class Classes(Collection):
 
     def get_all(self, class_name):
         """Returns all students in the corresponding class."""
-        # check if class exists
-            
         # retrieve class_id
-        query = f"""
-                SELECT 'class_id'
-                FROM '{self._tblname}'
-                WHERE class_name = ?
-                """
-        values = tuple(class_name)
-        rows = self._return(query, values)
-        class_id = rows[0]    # tuple
+        query = self._retrieve("class_id", self._tblname, "class_name", class_name)
+        row = self._return(query)
+        class_id = row["class_id"]
+
+        # check if class exists
+        if class_id is None:
+            return None
         
         # retrieve student_id and student_name
         query = f"""
-                SELECT 'student_Id', 'student_name'
+                SELECT 'student_id', 'student_name'
                 FROM 'Students-Classes'
                 WHERE class_id = ?
                 ORDER BY 'student_id' ASC
                 """
-        values = class_id
-        rows = self._return(query, values)
+        values = tuple(class_id)
+        row = self._return(query, values)
         
         data = {}
-        for item in rows:
-            data[item[0]] = item[1]
+        for item in row:
+            data[item["student_id"]] = item["student_name"]
         return data    # dictionary(key=id, value=student_name)
 
     def update(self):
@@ -151,28 +164,32 @@ class CCAs(Collection):
                 WHERE student_name = ?
                 """
         values = tuple(student_name)
-        rows = self._return(query, values)
-        student_id = rows[0]    # tuple
+        row = self._return(query, values)
+        student_id = row["student_id"]
 
+        # check if student exists
+        if student_id is None:
+            return None
+        
         # retrieve cca_id
         query = """
                 SELECT 'cca_id'
                 FROM 'Students-CCAs'
                 WHERE student_id = ?
                 """
-        values = student_id
-        rows = self._return(query, values)
-        cca_id = rows[0]    # tuple
+        values = tuple(student_id)
+        row = self._return(query, values)
+        cca_id = row["cca_id"]
 
         # retrive cca_name
         query = """
                 SELECT 'cca_name'
-                FROM 'Ccas'
+                FROM 'CCAs'
                 WHERE cca_id = ?
                 """
-        values = cca_id
-        rows = self._return(query, values)
-        cca_name = rows[0][0]
+        values = tuple(cca_id)
+        row = self._return(query, values)
+        cca_name = row["cca_name"]
 
         data = {}
         data[student_name] = cca_name
@@ -180,17 +197,20 @@ class CCAs(Collection):
 
     def get_all(self, cca_name):
         """Returns a CCA's details."""
+        # CHECK IF CCA EXITSTS
         query = """
                 SELECT *
                 FROM 'CCAs'
                 WHERE cca_name = ?
                 """
         values = tuple(cca_name)
-        rows = self._return(query, values)
-        ##############################
+        row = self._return(query, values)
+        field_names = row.keys()
 
-
-    # get_all method returns cca info
+        data = {}
+        for i, elem in enumerate(field_names):
+            data{elem} = row[i]
+        return data # dictionary
 
 class Activities(Collection):
     """
